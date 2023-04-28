a9cf6f0e371b66d92f62a91c080b17ddc99afb21
Johnny Proano <jproano22@yahoo.com>
2023-04-28 11:49:45 -0400

    added copy to clipboard functionality

--- index.js
+++ index.js
@@ -3,10 +3,23 @@ const hexBottom = document.getElementById("hex-bottom");
 const colorScheme = document.getElementById("color-scheme");
 const colorPicker = document.getElementById("color-picker");
 const submitBtn = document.getElementById("submit");
+let message = document.getElementById("message");
+
 let colorScemeValue = ``;
 let colorHexValue = ``;
 
 submitBtn.addEventListener("submit", handleSubmit);
+
+document.addEventListener("click", function (e) {
+  message.style.display = "initial";
+  let dataElement = e.target.src;
+  dataElement = `#${dataElement.slice(-6)}`;
+  navigator.clipboard.writeText(dataElement);
+  message.innerHTML = "-- Copied --";
+  setTimeout(function () {
+    message.innerHTML = "Click To Copy";
+  }, 1000);
+});
 
 function initialRender() {
   fetch(`https://www.thecolorapi.com/scheme?hex=F55A5A&mode=monochrome`)
@@ -35,7 +48,7 @@ function handleSubmit(event) {
     .then((data) => {
       data.colors.forEach((color) => {
         colorHtml += `
-          <img class="color-image" src="${color.image.bare}">`;
+          <img class="color-image" data-hex=${color.hex.value} src="${color.image.bare}">`;
         colorHexHtml += `
           <p class="color-hex">${color.hex.value}</p>`;
       });
